ce6f42501b1a3d99c0dd26fa3cc6bb91bc8b346e
Roald Ong <roald.ong@gmail.com>
2024-06-11 16:25:33 +0800

    Add in pcl and transform publisher

--- include/umeyama_test/umeyama.hpp
+++ include/umeyama_test/umeyama.hpp
@@ -3,6 +3,13 @@
 #include <rclcpp/rclcpp.hpp>
 #include <iostream>
 #include <chrono>
+#include <tf2_ros/transform_broadcaster.h>
+#include <pcl-1.12/pcl/point_cloud.h>
+#include <pcl-1.12/pcl/point_types.h>
+#include <pcl-1.12/pcl/registration/transformation_estimation_svd.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include "tf2/LinearMath/Quaternion.h"
+
 using namespace std::chrono_literals;
 
 class Umeyama : public rclcpp::Node
@@ -12,10 +19,7 @@ public:
 
     void timerCallback();
 
-
-
-    private:
-
-
+private:
     rclcpp::TimerBase::SharedPtr m_timer;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> m_tf_broadcaster;
 };
--- src/umeyama.cpp
+++ src/umeyama.cpp
@@ -2,13 +2,33 @@
 
 Umeyama::Umeyama() : Node("umeyama")
 {
-
-    std::cout << "start!\n";
     m_timer = this->create_wall_timer(
         500ms, std::bind(&Umeyama::timerCallback, this));
+
+    m_tf_broadcaster =
+        std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 }
 
 void Umeyama::timerCallback()
 {
-    std::cout << "timerCallback\n";
+    geometry_msgs::msg::TransformStamped t;
+
+    // Read message content and assign it to
+    // corresponding tf variables
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "target_frame";
+    t.child_frame_id = "source_frame";
+
+    t.transform.translation.x = 1.0;
+    t.transform.translation.y = 0;
+    t.transform.translation.z = 1;
+
+    tf2::Quaternion q;
+    q.setRPY(0.1, 0.2, 0.3);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    m_tf_broadcaster->sendTransform(t);
 }
