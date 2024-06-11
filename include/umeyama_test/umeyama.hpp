c00e8a6a4a5aaffa4884eb86adb75d0910c92a95
Roald Ong <roald.ong@gmail.com>
2024-06-11 21:21:13 +0800

    Add in rectangle visualization

--- include/umeyama_test/umeyama.hpp
+++ include/umeyama_test/umeyama.hpp
@@ -7,7 +7,9 @@
 #include <pcl-1.12/pcl/point_cloud.h>
 #include <pcl-1.12/pcl/point_types.h>
 #include <pcl-1.12/pcl/registration/transformation_estimation_svd.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 #include "tf2/LinearMath/Quaternion.h"
 
 using namespace std::chrono_literals;
@@ -22,4 +24,7 @@ public:
 private:
     rclcpp::TimerBase::SharedPtr m_timer;
     std::unique_ptr<tf2_ros::TransformBroadcaster> m_tf_broadcaster;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_source_pub;
+    geometry_msgs::msg::TransformStamped m_t;
+    sensor_msgs::msg::PointCloud2 m_rect_cloud;
 };
--- src/umeyama.cpp
+++ src/umeyama.cpp
@@ -7,28 +7,47 @@ Umeyama::Umeyama() : Node("umeyama")
 
     m_tf_broadcaster =
         std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+    m_source_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        "source_cloud", 10);
+    m_t.transform.translation.x = 1.0;
+    m_t.transform.translation.y = 0;
+    m_t.transform.translation.z = 1;
+
+    tf2::Quaternion q;
+    q.setRPY(0.1, 0.2, 0.3);
+    m_t.transform.rotation.x = q.x();
+    m_t.transform.rotation.y = q.y();
+    m_t.transform.rotation.z = q.z();
+    m_t.transform.rotation.w = q.w();
+    m_t.header.frame_id = "target_frame";
+    m_t.child_frame_id = "source_frame";
+
+    std::vector<double> y_coord = {0.5, 0.5, -0.5, -0.5};
+    std::vector<double> z_coord = {0.5, 0, 0, 0.5};
+    double x_coord = 1.0;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    for (int i = 0; i < 4; i++)
+    {
+        pcl::PointXYZ point;
+        point.x = x_coord;
+        point.y = y_coord[i];
+        point.z = z_coord[i];
+        cloud->push_back(point);
+    }
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl::toPCLPointCloud2(*cloud, pcl_pc2);
+    m_rect_cloud = sensor_msgs::msg::PointCloud2();
+    pcl_conversions::fromPCL(pcl_pc2, m_rect_cloud);
 }
 
 void Umeyama::timerCallback()
 {
-    geometry_msgs::msg::TransformStamped t;
 
-    // Read message content and assign it to
-    // corresponding tf variables
-    t.header.stamp = this->get_clock()->now();
-    t.header.frame_id = "target_frame";
-    t.child_frame_id = "source_frame";
+    m_t.header.stamp = this->get_clock()->now();
 
-    t.transform.translation.x = 1.0;
-    t.transform.translation.y = 0;
-    t.transform.translation.z = 1;
-
-    tf2::Quaternion q;
-    q.setRPY(0.1, 0.2, 0.3);
-    t.transform.rotation.x = q.x();
-    t.transform.rotation.y = q.y();
-    t.transform.rotation.z = q.z();
-    t.transform.rotation.w = q.w();
-
-    m_tf_broadcaster->sendTransform(t);
+    m_tf_broadcaster->sendTransform(m_t);
+    m_rect_cloud.header.stamp = this->get_clock()->now();
+    m_rect_cloud.header.frame_id = "source_frame";
+    m_source_pub->publish(m_rect_cloud);
 }
